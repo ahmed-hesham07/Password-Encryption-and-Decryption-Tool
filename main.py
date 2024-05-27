aa46ca9d49448c761e9d9321a571c5ac780de50a
Ahmed Hesham <A.Ismail02003@student.aast.edu>
2024-05-27 17:35:38 +0300

    adding the source code

---
+++ main.py
@@ -0,0 +1,87 @@
+from cryptography.fernet import Fernet
+import pandas as pd
+# Generate a key for encryption and decryption
+# This key should be stored securely
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+df = pd.DataFrame(columns=["password", "encrypted_password", "decrypted_password", "key"])
+getpass = str()
+
+
+
+def encrypt_password(password):
+    # Encrypt the password
+    encrypted_password = cipher_suite.encrypt(password.encode())
+    return encrypted_password
+
+
+def decrypt_password(encrypted_password):
+    # Decrypt the password
+    decrypted_password = cipher_suite.decrypt(encrypted_password).decode()
+    return decrypted_password
+
+
+def main():
+    # Encrypting the password
+    password = input("Enter a password to encrypt: ")
+    encrypted_password = encrypt_password(password)
+    print(f"Encrypted password: {encrypted_password.decode()}")
+
+
+    # Decrypting the password
+    decrypted_password = decrypt_password(encrypted_password)
+    print(f"Decrypted password: {decrypted_password}")
+
+    new_row = {
+        "password": password,
+        "encrypted_password": encrypted_password.decode(),
+        "decrypted_password": decrypted_password,
+        "key": key.decode()
+    }
+
+    # append the row to the DataFrame
+    global df
+    df = df._append(new_row, ignore_index=True)
+
+
+
+print("welecome to password encryption and decryption")
+print ("1. Add password")
+print ("2. Get password")
+print ("3. Exit")
+choice = input("Enter your choice: ")
+if choice == "1":
+    main()
+
+elif choice == "2":
+    getpass = input("enter your encrypted password: ")
+    position = df.set_index('encrypted_password', inplace=True)
+    position = df.index.get_loc(getpass)
+    print(df.iloc[position])
+elif choice == "3":
+    exit()
+else:
+    print("Invalid choice")
+
+choice = input("Do you want to continue? (yes/no): ")
+
+while choice == "yes":
+    print("1. Add password")
+    print("2. Get password")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        main()
+    elif choice == "2":
+        getpass = input("enter your encrypted password: ")
+        position = df.set_index('encrypted_password', inplace=True)
+        position = df.index.get_loc(getpass)
+        print(df.iloc[position])
+    elif choice == "3":
+        break
+    else:
+        print("Invalid choice")
+
+    choice = input("Do you want to continue? (yes/no): ")
+    if choice.lower() != "yes":
+        break
